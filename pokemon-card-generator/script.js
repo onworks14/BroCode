b2ba2a73b184e4e92ff7bac4ea4c9bbd4791d7b1
onworks14 <150022695+onworks14@users.noreply.github.com>
2024-03-16 06:30:59 +0000

    Compressed file via toptal

--- pokemon-card-generator/script.js
+++ pokemon-card-generator/script.js
@@ -1,25 +1,25 @@
-const typeColor={bug:"#26de81",dragon:"#ffeaa7",electric:"#fed330",fairy:"#FF0069",fighting:"#30336b",fire:"#f0932b",flying:"#81ecec",grass:"#00b894",ground:"#EFB549",ghost:"#a55eea",ice:"#74b9ff",normal:"#95afc0",poison:"#6c5ce7",psychic:"#a29bfe",rock:"#2d3436",water:"#0190FF"};const url=" https://pokeapi.co/api/v2/pokemon/";const card=document.getElementById("card");const btn=document.getElementById("btn");let getPokeData=()=>{let id=Math.floor(Math.random()*150)+1;const finalUrl=url+id;fetch(finalUrl).then(response=>response.json()).then(data=>{generateCard(data)})};let generateCard=data=>{const hp=data.stats[0].base_stat;const imgSrc=data.sprites.other.dream_world.front_default;const pokeName=data.name[0].toUpperCase()+data.name.slice(1);const statAttack=data.stats[1].base_stat;const statDefense=data.stats[2].base_stat;const statSpeed=data.stats[5].base_stat;const themeColor=typeColor[data.types[0].type.name];console.log(themeColor);card.innerHTML=`
+const typeColor={bug:"#26de81",dragon:"#ffeaa7",electric:"#fed330",fairy:"#FF0069",fighting:"#30336b",fire:"#f0932b",flying:"#81ecec",grass:"#00b894",ground:"#EFB549",ghost:"#a55eea",ice:"#74b9ff",normal:"#95afc0",poison:"#6c5ce7",psychic:"#a29bfe",rock:"#2d3436",water:"#0190FF"},url=" https://pokeapi.co/api/v2/pokemon/",card=document.getElementById("card"),btn=document.getElementById("btn");let getPokeData=()=>{let e=Math.floor(150*Math.random())+1,t=" https://pokeapi.co/api/v2/pokemon/"+e;fetch(t).then(e=>e.json()).then(e=>{generateCard(e)})},generateCard=e=>{let t=e.stats[0].base_stat,a=e.sprites.other.dream_world.front_default,s=e.name[0].toUpperCase()+e.name.slice(1),r=e.stats[1].base_stat,p=e.stats[2].base_stat,n=e.stats[5].base_stat,d=typeColor[e.types[0].type.name];console.log(d),card.innerHTML=`
       <p class="hp">
         <span>HP</span>
-          ${hp}
+          ${t}
       </p>
-      <img src=${imgSrc} />
-      <h2 class="poke-name">${pokeName}</h2>
+      <img src=${a} />
+      <h2 class="poke-name">${s}</h2>
       <div class="types">
 
       </div>
       <div class="stats">
         <div>
-          <h3>${statAttack}</h3>
+          <h3>${r}</h3>
           <p>Attack</p>
         </div>
         <div>
-          <h3>${statDefense}</h3>
+          <h3>${p}</h3>
           <p>Defense</p>
         </div>
         <div>
-          <h3>${statSpeed}</h3>
+          <h3>${n}</h3>
           <p>Speed</p>
         </div>
       </div>
-`;appendTypes(data.types);styleCard(themeColor)};let appendTypes=types=>{types.forEach(item=>{let span=document.createElement("SPAN");span.textContent=item.type.name;document.querySelector(".types").appendChild(span)})};let styleCard=color=>{card.style.background=`radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;card.querySelectorAll(".types span").forEach(typeColor=>{typeColor.style.backgroundColor=color})};btn.addEventListener("click",getPokeData);window.addEventListener("load",getPokeData);
+`,appendTypes(e.types),styleCard(d)},appendTypes=e=>{e.forEach(e=>{let t=document.createElement("SPAN");t.textContent=e.type.name,document.querySelector(".types").appendChild(t)})},styleCard=e=>{card.style.background=`radial-gradient(circle at 50% 0%, ${e} 36%, #ffffff 36%)`,card.querySelectorAll(".types span").forEach(t=>{t.style.backgroundColor=e})};btn.addEventListener("click",getPokeData),window.addEventListener("load",getPokeData);
